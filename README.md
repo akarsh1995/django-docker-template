bf6675bfec3279a036a5c5788ae4a4c97f818444
Akarsh Jain <akarsh.1995.02@gmail.com>
2020-01-15 18:14:03 +0530

    added for postgres

---
+++ README.md
@@ -0,0 +1,13 @@
+# To use the package run
+
+```sh
+    $ docker-compose build
+    $ docker-compose up -d postgres
+    $ docker-compose run app setup_db
+```
+
+Docker run app after setting up db
+
+```
+    $ docker-compose up app
+```
--- app/sample/settings.py
+++ app/sample/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_DOCKER_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 

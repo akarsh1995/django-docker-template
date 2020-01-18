19ed874d9c063e57a7108e782941f6b7fe4aa6a6
Akarsh Jain <akarsh.1995.02@gmail.com>
2020-01-18 17:07:46 +0530

    startup script modified for detached processing

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 docker-compose build
 docker-compose up -d postgres
+# sleep to let postgres completely up.
 sleep 10
 docker-compose run app setup_db
-docker-compose up app
-docker-compose up nginx
+docker-compose up -d app
+docker-compose up -d nginx

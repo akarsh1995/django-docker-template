009f405d791cecaca8c91dc0bac1c8d2f82dc356
Akarsh Jain <akarsh.1995.02@gmail.com>
2020-01-23 17:33:56 +0530

    pre-alpha-deployment

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 docker-compose build
 docker-compose up -d postgres
 # sleep to let postgres completely up.
-sleep 10
+sleep 5s
 docker-compose run app setup_db
 docker-compose up -d app
-docker-compose up -d nginx
+docker-compose up -d encrypted_nginx
